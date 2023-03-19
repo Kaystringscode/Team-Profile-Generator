653425f84ca70ffb550ca62e1ec2ce8c796fb610
kaystringscode <kaystrings@gmail.com>
2023-03-19 22:35:39 +0000

    Employee Engineer Intern and Managers js code written

--- starter/__tests__/Employee.test.js
+++ starter/__tests__/Employee.test.js
@@ -1,5 +1,5 @@
 const Employee = require("../lib/Employee");
-
+const employee = new Employee ("Alice", "100", "test@test.com");
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
