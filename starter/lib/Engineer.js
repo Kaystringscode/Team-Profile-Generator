ea41e971b37f1fddeb535a51a1ecd556e93acbfa
kaystringscode <kaystrings@gmail.com>
2023-03-19 23:24:19 +0000

    inquirer setup complete

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// import Employee constructor 
+const Employee = require("./Employee");
+
+// engineer constructor extends employee constructor 
+class Engineer extends Employee {
+    constructor (name, id, email, github) {
+      
+        super (name, id, email);
+
+        this.github = github; 
+    }
+
+
+    getGithub () {
+        return this.github;
+    }
+
+  
+    getRole () {
+        return "Engineer";
+    }
+}
+
+// to be exported 
+module.exports = Engineer; 
